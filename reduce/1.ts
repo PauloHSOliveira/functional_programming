80ced03802001cab813aa8b8e85c2c9e60d844b8
paulooliveira <paulooliveiradev@gmail.com>
2021-07-28 16:45:04 -0300

    chore: create reduce example

---
+++ reduce/1.ts
@@ -0,0 +1,14 @@
+type OPERATORS = '*' | '+' | '-' | '/'
+
+const numbers = [1,2,3]
+
+const sum = (x:number,y:number) => x + y
+const multi = (x:number, y:number) => x*y
+
+const _sum = numbers.reduce(sum, 0)
+const _multi = numbers.reduce(multi, 0)
+
+console.log(_sum)
+console.log(_multi)
+
+export {}
